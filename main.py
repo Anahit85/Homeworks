a4163d08b91234254461a7b57bc17c134bfe11e7
Anahit85 <anahit.poghosyan@iths.se>
2025-10-20 13:15:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,126 @@
+#1
+age = 40
+print(age)
+
+#2
+years= 1985
+month= 'September'
+day= 8
+print (years," ", month, " ", day)
+
+
+
+#3
+a = 10
+b = 3.5
+print(a,b)
+print(type(a))
+print(type(b))
+
+
+#4
+num = '25'
+print(int(num))
+print(type(num))
+
+
+#5
+price= '19.99'
+print(float(price))
+print(type(price))
+
+#6
+temperature = '36.6'
+value = int(float(temperature))
+print(value)
+print(type(value))
+
+
+#7
+word = 'Python'
+print(word)
+
+#8
+name = 'Anahit'
+surname = 'Poghosyan'
+city = 'Stockholm'
+print(name, " " , surname, " ", city)
+
+#9
+e= 'Learning is fun, '
+print(e * 2)
+
+#10
+text= 'Hello, World!'
+print(len(text))
+
+#11
+text= 'Hello'
+print(text[0])
+print(text[-1])
+
+#12
+text='Programming'
+print(text[3])
+print(text[5])
+print(text[8])
+
+#13
+text= 'Armenia'
+print(text[:3])
+
+#14
+text='Armenia'
+print(text[:-2])
+
+#15
+text1='I love Python'
+text2= text1.replace('Python', 'coding')
+print(text2)
+
+#16
+word = 'hello'
+print(word.upper())
+
+#17
+text= 'good morning'
+print(word.upper())
+
+#18
+text= 'PYTHON IS FUN'
+print(text.lower())
+
+#19
+text= 'hello world'
+print(word.capitalize())
+
+#20
+fruits1= 'I love appels'
+fruits2= fruits1.replace('appels', 'bananas')
+print(fruits2)
+
+
+#21
+sentence= 'Hello world from Python'
+print(sentence.split(" "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
